a5c672e647fed19c25038adcfe5b6043c03aad44
onoie <onoie3@gmail.com>
2017-06-22 15:21:25 +0900

    add build.sh

---
+++ clisp/src/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+clisp -norc hello.lisp
+
--- clisp/src/hello.lisp
+++ clisp/src/hello.lisp
@@ -1,1 +1,9 @@
-(prin1 "Hello World!")
+(defun hello ()
+  (print "hello, world")
+  (ext:exit))
+
+(ext:saveinitmem "hello.exe"
+                 :quiet t               ; バナーを表示しない
+                 :norc t                ; 初期化ファイルをロードしない
+                 :init-function #'hello ; REPLの前にhelloを呼ぶ
+                 :executable t)
