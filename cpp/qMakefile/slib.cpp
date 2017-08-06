5a0a0339486c90e1c118b91f23fe05b64a1ce0a0
onoie <onoie3@gmail.com>
2017-08-06 11:40:20 +0900

    add Qt4 Makefile

---
+++ cpp/qMakefile/slib.cpp
@@ -0,0 +1,8 @@
+//
+// Created by onoie on 7/11/17.
+//
+
+#include <stdio.h>
+void hello(){
+    printf("Hello,World!\n");
+}
---
+++ cpp/qMakefile/two/hello.cc
@@ -0,0 +1,13 @@
+//#include <iostream>
+//int main(){
+//  std::cout << "\nHello World!\n";
+//}
+#include <QApplication>
+#include <QMainWindow>
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QMainWindow w;
+    w.show();
+    return app.exec();
+}
+
